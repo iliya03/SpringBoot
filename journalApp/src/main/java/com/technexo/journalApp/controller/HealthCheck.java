7ba751c7aefda199965260f5d4ccfe1888937484
iliya03 <iliyafathma@gmail.com>
2025-02-28 16:06:52 +0545

    journal app

---
+++ journalApp/src/main/java/com/technexo/journalApp/controller/HealthCheck.java
@@ -0,0 +1,13 @@
+package com.technexo.journalApp.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthCheck {
+
+   @GetMapping("/health-check")
+   public String healthcare() {
+      return "OK!";
+   }
+}
---
+++ journalApp/src/main/java/com/technexo/journalApp/entity/JournalEntry.java
@@ -0,0 +1,57 @@
+package com.technexo.journalApp.entity;
+
+import java.time.LocalDateTime;
+
+public class JournalEntry {
+
+    private long id;
+    private String title;
+    private String content;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    // Getter and Setter for id
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    // Getter and Setter for title
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    // Getter and Setter for content
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    // Getter and Setter for createdAt
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    // Getter and Setter for updatedAt
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(LocalDateTime updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+}
